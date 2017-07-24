886d402e24f337b6d9ed0ca4a9e672ed6b7f29f1
yaojenkuo <tonykuoyj@gmail.com>
2017-07-24 22:54:21 +0800

    added projects

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,52 @@
+class ProjectsController < ApplicationController
+    before_action :find_project, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @projects = Project.all.order("created_at desc")
+    end
+
+    def new
+        @project = Project.new
+    end
+
+    def create
+        @project = Project.new project_params
+
+        if @project.save
+            redirect_to @project, notice: "Project was successfully saved!"
+        else
+            render 'new'
+        end
+    end
+
+    def show
+    end
+
+    def edit
+    end
+    
+    def update
+        if @project.update project_params
+            redirect_to @project, notice: "Project was successfully updated."
+        else
+            render 'edit', notice: "Project was not updated."
+        end
+    end
+
+    def destroy
+        @project.destroy
+        redirect_to projects_path
+    end
+
+    private
+
+    def find_project
+        @project = Project.friendly.find(params[:id])
+    end
+
+    def project_params
+        params.require(:project).permit(:title, :description, :link, :slug)
+    end
+
+
+end
---
+++ app/models/project.rb
@@ -0,0 +1,4 @@
+class Project < ApplicationRecord
+    extend FriendlyId
+    friendly_id :title, use: :slugged
+end
